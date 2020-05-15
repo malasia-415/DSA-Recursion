3895e36741664332e95488a8c441ff0438db87eb
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-05-14 17:32:45 -0700

    Counting sheep questions

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function sheepCountDown(noun) {
   sheepCountDown(num -1);
 }
 sheepCountDown(10);
+
+
+1) Input of the program is the Integer of the number of sheep. (Number)
+2) Output of the program Console log of a message stating "Another sheep jumps over the fence" and the current sheep number
+3) The number of sheep decremented 1.
+4) Will print out the number of sheep as one less than before, until no sheep are left.
